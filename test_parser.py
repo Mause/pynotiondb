6cd4002150ba7c127fb30e50980c1272694d125d
Elliana May <me@mause.me>
2025-09-20 15:52:32 +0800

    add basic parser test

---
+++ test_parser.py
@@ -0,0 +1,20 @@
+from pytest import mark
+from pynotiondb.mysql_query_parser import MySQLQueryParser
+
+
+@mark.parametrize(
+    "sql",
+    [
+        "SELECT * FROM table",
+        # "SELECT column1, column2 FROM table WHERE column1 = 'value'",
+        "INSERT INTO table (column1, column2) VALUES ('value1', 'value2')",
+        "UPDATE table SET column1 = 'new_value' WHERE column2 = 'value2'",
+        "DELETE FROM table WHERE column1 = 'value1';",
+    ],
+)
+def test_sql_parser(sql: str, snapshot):
+    parser = MySQLQueryParser(sql)
+    ok, typ = parser.check_statement()
+    assert ok
+
+    snapshot.assert_match(parser.parse())
