1a6336c2fdf974989ae61b280d9f495203ec482e
Elliana May <me@mause.me>
2025-09-23 12:55:05 +0800

    fix up queries

--- test_parser.py
+++ test_parser.py
@@ -6,12 +6,12 @@ from pynotiondb.mysql_query_parser import MySQLQueryParser
 @mark.parametrize(
     "sql",
     [
-        "SELECT * FROM table",
-        "SELECT column1, column2 FROM table WHERE column1 = 'value'",
+        "SELECT * FROM table1",
+        "SELECT column1, column2 FROM table1 WHERE column1 = 'value'",
         "INSERT INTO table1 (column1, column2) VALUES ('value1', 'value2')",
         "UPDATE table SET column1 = 'new_value' WHERE column2 = 'value2'",
-        "DELETE FROM table WHERE column1 = 'value1';",
-        "SELECT * FROM table1 WHERE col1=1 AND col2='text' OR col3 IS NULL;",
+        "DELETE FROM table1 WHERE column1 = 'value1';",
+        "SELECT * FROM table1 WHERE column1=1 AND column2='text' OR column3 IS NULL;",
     ],
 )
 def test_sql_parser(sql: str, snapshot):
